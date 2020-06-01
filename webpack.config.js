a366de52bdb9e41ff71acffe27c08e77baf20a5e
Evgeniy <blednovea@smartguide.agency>
2020-06-01 19:56:01 +0300

    base config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const prod = process.env.NODE_ENV === 'production';
+const config = require('./etc/config');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', 'tsx'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.((woff(2)?)|ttf|eot|otf)(\?[a-z0-9#=&.]+)?$/,
+      use: 'file-loader'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: config.app.title,
+      template: 'src/index.html'
+    })
+  ],
+  ...(!prod ? {
+    devServer: {
+      port: config.dev.port,
+      open: false,
+      hot: true,
+      compress: true,
+      stats: 'errors-only',
+      overlay: true
+    }
+  } : {})
+};
